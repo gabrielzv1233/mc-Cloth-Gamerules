df551e48c5a9618898d5b82bc9f24d7950a9e3df
gabrielzv1233 <77300296+gabrielzv1233@users.noreply.github.com>
2026-09-05 22:50:30 -0500

    Port Mod Menu integration to Minecraft 26.2

--- src/main/java/tk/estecka/clothgamerules/ModMenu.java
+++ src/main/java/tk/estecka/clothgamerules/ModMenu.java
@@ -2,20 +2,25 @@ package tk.estecka.clothgamerules;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.resource.featuretoggle.FeatureSet;
-import net.minecraft.text.Text;
-import net.minecraft.world.rule.GameRules;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.level.gamerules.GameRules;
 import tk.estecka.clothgamerules.api.ClothGamerulesScreenFactory;
 
 public class ModMenu
 implements ModMenuApi
 {
 	public ConfigScreenFactory<?> getModConfigScreenFactory(){
-		return parent -> getModConfigScreen(parent);
+		return this::getModConfigScreen;
 	}
 
-	public Screen	getModConfigScreen(Screen parent){
-		return ClothGamerulesScreenFactory.CreateScreen(parent, Text.literal("Test Screen"), new GameRules(FeatureSet.empty()), noop->{});
+	public Screen getModConfigScreen(Screen parent){
+		return ClothGamerulesScreenFactory.CreateScreen(
+			parent,
+			Component.literal("Test Screen"),
+			new GameRules(FeatureFlags.REGISTRY.allFlags()),
+			noop->{}
+		);
 	}
 }

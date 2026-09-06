913ea9ca54b49889811f05e8847367cf1a0c33f1
gabrielzv1233 <77300296+gabrielzv1233@users.noreply.github.com>
2026-09-05 22:50:38 -0500

    Port development gamerules to 26.2

--- src/main/java/tk/estecka/clothgamerules/TestRules.java
+++ src/main/java/tk/estecka/clothgamerules/TestRules.java
@@ -1,12 +1,10 @@
 package tk.estecka.clothgamerules;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.gamerule.v1.CustomGameRuleCategory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleBuilder;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.level.gamerules.GameRuleCategory;
 
 public class TestRules
 implements ModInitializer
@@ -21,24 +19,18 @@ implements ModInitializer
 	public void onInitialize() {
 		if (FabricLoader.getInstance().isDevelopmentEnvironment())
 		{
-			CustomGameRuleCategory fabricCategory = new CustomGameRuleCategory(Identifier.of("cloth-gamerules", "test_rules"), Text.literal("Test Rules"));
-			GameRuleBuilder.forBoolean(true).category(fabricCategory).buildAndRegister(Identifier.of("clothrule", "bool"));
-
-			GameRuleBuilder.forInteger(1)          .category(fabricCategory).buildAndRegister(Identifier.of("clothrule", "int"));
-			GameRuleBuilder.forDouble(1.0)         .category(fabricCategory).buildAndRegister(Identifier.of("clothrule", "double"));
-			GameRuleBuilder.forEnum(ETestEnum.SOME).category(fabricCategory).buildAndRegister(Identifier.of("clothrule", "enum"));
+			GameRuleCategory fabricCategory = new GameRuleCategory(Identifier.fromNamespaceAndPath("cloth-gamerules", "test_rules"));
+			GameRuleBuilder.forBoolean(true).category(fabricCategory).buildAndRegister(Identifier.fromNamespaceAndPath("clothrule", "bool"));
+			GameRuleBuilder.forInteger(1).category(fabricCategory).buildAndRegister(Identifier.fromNamespaceAndPath("clothrule", "int"));
+			GameRuleBuilder.forDouble(1.0).category(fabricCategory).buildAndRegister(Identifier.fromNamespaceAndPath("clothrule", "double"));
+			GameRuleBuilder.forEnum(ETestEnum.SOME).category(fabricCategory).buildAndRegister(Identifier.fromNamespaceAndPath("clothrule", "enum"));
 
 			GameRuleBuilder.forInteger(1).range(0, 16)
-				.category(fabricCategory).buildAndRegister(Identifier.of("clothrule", "int.bounded"))
-				;
+				.category(fabricCategory).buildAndRegister(Identifier.fromNamespaceAndPath("clothrule", "int.bounded"));
 			GameRuleBuilder.forDouble(1.0).range(0.1, 9.99)
-				.category(fabricCategory).buildAndRegister(Identifier.of("clothrule", "double.bounded"))
-				;
+				.category(fabricCategory).buildAndRegister(Identifier.fromNamespaceAndPath("clothrule", "double.bounded"));
 			GameRuleBuilder.forEnum(ETestEnum.SOME).supportedValues(ETestEnum.NONE, ETestEnum.SOME)
-				.category(fabricCategory).buildAndRegister(Identifier.of("clothrule", "enum.bounded"))
-				;
-
+				.category(fabricCategory).buildAndRegister(Identifier.fromNamespaceAndPath("clothrule", "enum.bounded"));
 		}
-
 	}
 }
